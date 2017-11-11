9f0d2549e1b611d14577829973b8b63c25f70f4e
ecrofeg <ecrofeg1@gmail.com>
2017-11-11 22:48:47 +0300

    TypeScript init + Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const moduleName = 'desk';
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const isDevMode = process.env.NODE_ENV === 'dev';
+
+const extractSass = new ExtractTextPlugin({
+	filename: `${moduleName}.min.css`
+});
+
+const config = {
+	context: __dirname,
+	entry: './src/main.ts',
+	watch: isDevMode,
+
+	watchOptions: {
+		ignored: /node_modules/,
+		aggregateTimeout: 300
+	},
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: `${moduleName}.min.js`
+	},
+
+	resolve: {
+		modules: [
+			'node_modules',
+			path.resolve(__dirname, 'src'),
+			path.resolve(__dirname, 'dist')
+		],
+		extensions: ['.ts', '.js', '.json', '.jsx', '.tsx', '.css', '.scss']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				include: [
+					path.resolve(__dirname, 'src/css')
+				],
+				use: extractSass.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								minimize: !isDevMode
+							}
+						},
+						'resolve-url-loader',
+						'postcss-loader',
+						'sass-loader'
+					],
+					fallback: 'style-loader',
+					publicPath: path.resolve(__dirname, 'dist')
+				})
+			}
+		]
+	},
+
+	plugins: [
+		extractSass,
+		new webpack.NoEmitOnErrorsPlugin()
+	]
+};
+
+config.plugins.push(
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify(isDevMode ? 'development' : 'production')
+		}
+	})
+);
+
+module.exports = config;
