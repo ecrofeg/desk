346539db7687bb2f4baee8e44cf8157340abb65f
ecrofeg <ecrofeg1@gmail.com>
2017-11-22 01:07:49 +0400

    Project by ID

--- dist/routes/api/project.js
+++ dist/routes/api/project.js
@@ -22,4 +22,23 @@ router.get('/', function (req, res) {
         }
     });
 });
+router.get('/:id', function (req, res) {
+    var projectId = req.params.id;
+    var cacheKey = "project_" + projectId;
+    redis_1.default.get(cacheKey, function (error, cachedData) {
+        if (cachedData) {
+            res.send(cachedData);
+        }
+        else {
+            db_1.default.query("\n\t\t\t\tSELECT project.name, project.description, project.created_at, project.updated_at\n\t\t\t\tFROM project\n\t\t\t\tWHERE project.id=? \n\t\t\t\tLIMIT 1", projectId, function (error, results) {
+                var project = null;
+                if (results instanceof Array && results.length) {
+                    project = results.shift();
+                    redis_1.default.set(cacheKey, JSON.stringify(project));
+                }
+                res.json(project);
+            });
+        }
+    });
+});
 exports.default = router;
--- server/routes/api/project.ts
+++ server/routes/api/project.ts
@@ -27,4 +27,34 @@ router.get('/', (req: express.Request, res: express.Response) => {
 	});
 });
 
+router.get('/:id', (req: express.Request, res: express.Response) => {
+	const { id: projectId } = req.params;
+	const cacheKey: string = `project_${projectId}`;
+
+	redis.get(cacheKey, (error, cachedData) => {
+		if (cachedData) {
+			res.send(cachedData);
+		}
+		else {
+			db.query(`
+				SELECT project.name, project.description, project.created_at, project.updated_at
+				FROM project
+				WHERE project.id=? 
+				LIMIT 1`,
+				projectId,
+				(error: mysql.MysqlError | null, results: any) => {
+					let project: any = null;
+
+					if (results instanceof Array && results.length) {
+						project = results.shift();
+						redis.set(cacheKey, JSON.stringify(project));
+					}
+
+					res.json(project);
+				}
+			);
+		}
+	});
+});
+
 export default router;
